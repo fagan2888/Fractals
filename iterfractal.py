704874122452f15a9ba420e2c5dd4fc63fe53ebf
Vitchyr <vitchyr@gmail.com>
2013-10-13 21:46:54 -0400

    Completed Mandlebrot and Julia set generation. Outputs to file.

--- iterfractal.py
+++ iterfractal.py
@@ -1,53 +1,66 @@
 import math
 import pygame
 
-def mandlebrot(x_0, ci, cj, x_range, y_range, depth):
+def julia(x_0, ci, cj, x_range, y_range, di, dj, depth, file):
     i = float(-x_range) #i = Re{x}
-    di = -i/depth
     j = float(-y_range) #j = Im{x}
-    dj = -j/depth
     #check grid of points
     while j < y_range:
         i = float(-x_range)
         while i < x_range:
-            if mandlebrot_point_in(i,j,c_real,c_img):
-                #draw_point(i,j)
-                print("IN: (%3f, %3f)" % (i, j))
+            if julia_point_in(i, j, c_real, c_img, depth):
+                file.write("%5f\t%5f\n" % (i, j))
             i += di
-            #pygame.display.update()
         j += dj
 
 #TO DO      
 def draw_point(x,y):
     point = pygame.Rect(x,y,1,1)
-    pygame.draw.rect(surface, black, point)
+    pygame.draw.rect(surface, white, point)
 
-#toggle max depth and magnitude limit for cut-off
-max_depth = 5
-upper_limit = 2
-def mandlebrot_point_in(x, y, c_real, c_img, depth = max_depth):
+def julia_point_in(x, y, c_real, c_img, depth = 5):
     if depth > 0:
         if x**2 + y**2 >= upper_limit:
             return False
         #using x_n+1 = x_n^2 + c with complex values
         #(a+ib)^2 + c= a^2 + 2iab - b^2 + c = a^2 - b^2 + c_real + i(2ab+c_img)
         #TO DO:  Use complex library to simplify this
-        return mandlebrot_point_in(x**2 - y**2 + c_real, 2*x*y + c_img, c_real,
+        return julia_point_in(x**2 - y**2 + c_real, 2*x*y + c_img, c_real,
                                    c_img, depth-1)
     return True
 
-##pygame.init()
+def mandlebrot(real_range, img_range, dc_real, dc_img, depth, file):
+    c_real = float(-real_range) #i = Re{x}
+    c_img = float(-img_range) #j = Im{x}
+    #check grid of points
+    while c_img < img_range:
+        c_real = float(-real_range)
+        while c_real < real_range:
+            if julia_point_in(0, 0, c_real, c_img, depth):
+                file.write("%5f\t%5f\n" % (c_real, c_img))
+            c_real += dc_real
+        c_img += dc_img
 
-screen_height = 600
-screen_width = 800
+# File Saving Settings
+julia_f = open('julia.dat', 'w')
+mandlebrot_f = open('mandlebrot.dat', 'w')
 
-##black = pygame.Color(0,0,0,255)
-##surface = pygame.Surface((height,width))
-##
-##window = pygame.display.set_mode((width, height))
-y_range = 2
+# Fractal Detail Settings
 x_range = 2
+dx = 0.002
+y_range = 2
+dy = 0.002
+depth = 25
+upper_limit = 5
+
+#Julia Set Settings
 x_0 = 0
-c_real = 0.3
-c_img = 0.1
-mandlebrot(x_0, c_real, c_img, x_range, y_range, 100)
+c_real = -.8
+c_img = .156
+#julia(x_0, c_real, c_img, x_range, y_range, dx, dy, depth, julia_f)
+
+#Mandlebrot Set
+mandlebrot(x_range, y_range, dx, dy, depth, mandlebrot_f)
+
+julia_f.close()
+mandlebrot_f.close()
