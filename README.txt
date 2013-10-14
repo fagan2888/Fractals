8e5c6ff2768ed4fe7e8890f803a1f9a557e99adc
Vitchyr <vitchyr@gmail.com>
2013-10-13 21:55:43 -0400

    Added a README.txt

---
+++ README.txt
@@ -0,0 +1,12 @@
+Author: Vitchyr Pong
+
+Create fractals!
+
+Use fractals.py to generate the Sherpinsky triangle (and the Koch snowflake
+eventually) fractal using pygame.
+
+Using iterfractal.py to output Julia sets as well as the Mandlebrot set to a
+.dat file.
+
+Feel free to use any of my code to play, impress your friends, make money, or
+conquer the world. Please just cite me. :) Have fun!
--- fractals.py
+++ fractals.py
@@ -2,7 +2,7 @@ import turtle
 import math
 from ColorReference import ColorReference
 
-def sherp(depth, distance):
+def sherp(depth, distance, colorRef):
     if depth > -1:
         turtle.fillcolor(colorRef.getColor(depth))
         turtle.fill(True)
@@ -14,7 +14,7 @@ def sherp(depth, distance):
         for x in range(3):
             turtle.fd(distance) #continue the sherpinski
             turtle.lt(120) #real sherpinski
-            if depth == 
+            if depth == 0:
                 turtle.fill(True)
             sherp(depth-1, distance/2)
             #turtle.lt(120) #messed up sherpinski
@@ -50,7 +50,7 @@ turtle.sety(-distance * math.sqrt(3) / 4)   #1/2 way up triangle
 turtle.pendown()
 turtle.colormode(255)
 
-koch(depth, distance)
+#koch(depth, distance)
 
-#colorRef = ColorReference(depth)
-#sherp(depth, distance)
+colorRef = ColorReference(depth)
+sherp(depth, distance, colorRef)
